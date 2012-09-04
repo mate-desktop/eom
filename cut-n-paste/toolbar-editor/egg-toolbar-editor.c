4c2b90806ffce73f9f5ad861df939f119256cd74
Steve Zesch <stevezesch2@gmail.com>
2012-09-04 12:56:45 -0400

    Fixed incorrect fsf addresses. Closes #4.

--- cut-n-paste/toolbar-editor/egg-toolbar-editor.c
+++ cut-n-paste/toolbar-editor/egg-toolbar-editor.c
@@ -13,7 +13,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  *  $Id: egg-toolbar-editor.c 929 2009-02-19 14:49:56Z friemann $
  */
