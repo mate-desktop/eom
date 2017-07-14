f7ca9dc7fcc5e088fad181e6f76c5e8a47d70a22
Bastien Nocera <hadess@hadess.net>
2017-07-14 11:41:38 +0200

    thumbnail: Remove use of mate-desktop-thumbnail helper
    don't use mate_desktop_thumbnail_scale_down_pixbuf().
    gdk-pixbuf doesn't have the same bugs when shrinking images by large
    factors.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=784942
    origin commit: https://gitlab.gnome.org/GNOME/eog/commit/0e7749b

--- src/eom-thumbnail.c
+++ src/eom-thumbnail.c
@@ -123,9 +123,10 @@ create_thumbnail_from_pixbuf (EomThumbData *data,
 
 	perc = CLAMP (128.0/(MAX (width, height)), 0, 1);
 
-	thumb = mate_desktop_thumbnail_scale_down_pixbuf (pixbuf,
-							   width*perc,
-							   height*perc);
+	thumb = gdk_pixbuf_scale_simple (pixbuf,
+	                                 width*perc,
+	                                 height*perc,
+	                                 GDK_INTERP_HYPER);
 
 	return thumb;
 }
@@ -444,7 +445,7 @@ eom_thumbnail_fit_to_size (GdkPixbuf *thumbnail, gint dimension)
 		width  = MAX (width  * factor, 1);
 		height = MAX (height * factor, 1);
 
-		result_pixbuf = mate_desktop_thumbnail_scale_down_pixbuf (thumbnail, width, height);
+		result_pixbuf = gdk_pixbuf_scale_simple (thumbnail, width, height, GDK_INTERP_HYPER);
 
 		return result_pixbuf;
 	}
