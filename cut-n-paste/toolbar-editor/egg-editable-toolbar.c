a691b5dc229c5adb98754ff4e0ead82f94bb36f4
Pablo Barciela <scow@riseup.net>
2019-09-19 09:54:45 +0200

    egg_editable_toolbar: avoid 'g_type_class_add_private'

--- cut-n-paste/toolbar-editor/egg-editable-toolbar.c
+++ cut-n-paste/toolbar-editor/egg-editable-toolbar.c
@@ -57,8 +57,6 @@ enum
 
 static guint egg_editable_toolbar_signals[LAST_SIGNAL] = { 0 };
 
-#define EGG_EDITABLE_TOOLBAR_GET_PRIVATE(object)(G_TYPE_INSTANCE_GET_PRIVATE ((object), EGG_TYPE_EDITABLE_TOOLBAR, EggEditableToolbarPrivate))
-
 struct _EggEditableToolbarPrivate
 {
   GtkUIManager *manager;
@@ -81,7 +79,7 @@ struct _EggEditableToolbarPrivate
   GtkToolItem *dnd_toolitem;
 };
 
-G_DEFINE_TYPE (EggEditableToolbar, egg_editable_toolbar, GTK_TYPE_BOX)
+G_DEFINE_TYPE_WITH_PRIVATE (EggEditableToolbar, egg_editable_toolbar, GTK_TYPE_BOX)
 
 static int
 get_dock_position (EggEditableToolbar *etoolbar,
@@ -1360,7 +1358,7 @@ egg_editable_toolbar_init (EggEditableToolbar *etoolbar)
 
   EggEditableToolbarPrivate *priv;
 
-  priv = etoolbar->priv = EGG_EDITABLE_TOOLBAR_GET_PRIVATE (etoolbar);
+  priv = etoolbar->priv = egg_editable_toolbar_get_instance_private (etoolbar);
 
   priv->save_hidden = TRUE;
 
@@ -1636,8 +1634,6 @@ egg_editable_toolbar_class_init (EggEditableToolbarClass *klass)
 							 "Edit Mode",
 							 FALSE,
 							 G_PARAM_READWRITE | G_PARAM_STATIC_NAME | G_PARAM_STATIC_NICK | G_PARAM_STATIC_BLURB));
-
-  g_type_class_add_private (object_class, sizeof (EggEditableToolbarPrivate));
 }
 
 GtkWidget *
